9e1a8dc10f18adc45ac78edd5a16f544d4a94972
CupapiOT <166366567+CupapiOT@users.noreply.github.com>
2024-10-15 20:52:08 +0800

    Refactor line numbering

--- init.lua
+++ init.lua
@@ -99,14 +99,11 @@ vim.g.have_nerd_font = false
 --  For more options, you can see `:help option-list`
 
 -- Make line numbers default
--- vim.opt.number = true
+vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
-
--- Hybrid mode
 vim.opt.relativenumber = true
-vim.opt.number = true
+-- Activate hybrid line numbers by activating both number and relativenumber.
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
