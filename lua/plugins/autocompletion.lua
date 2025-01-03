647db73f64e49d2a4bb454389d684ffd49966961
CupapiOT <166366567+CupapiOT@users.noreply.github.com>
2025-01-03 16:30:40 +0800

    Add HTML Boilerplate

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -179,6 +179,54 @@ return { -- Autocompletion
             --]]
           ),
         })
+        ls.add_snippets('html', {
+          s(
+            'html-boilerplate',
+            fmt(
+              [[
+              <!doctype html>
+              <html lang="{}">
+                <head>
+                  <meta charset="UTF-8" />
+                  <title>{}</title>
+                  <meta name="viewport" content="width=device-width,initial-scale=1" />
+                  <meta name="description" content="{}" />
+                  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                  <link rel="icon" href="./{}" />
+                  <link rel="stylesheet" href="./{}" />
+                </head>
+                <body>
+                  <main>
+                    {}
+                  </main>
+                  <script src="./{}"></script>
+                </body>
+              </html>
+              ]],
+              { i(0, 'en'), i(1, 'Title for Page'), i(2, 'DESCRIPTION'), i(3, 'favicon.ico'), i(4, 'css/style.css'), i(5, '<h1>Hello, world!</h1>'), i(6, 'js/script.js') }
+            )
+            --[[
+            <!doctype html>
+            <html lang="{en}">
+              <head>
+                <meta charset="UTF-8" />
+                <title>{Hello, world!}</title>
+                <meta name="viewport" content="width=device-width,initial-scale=1" />
+                <meta name="description" content="{DESCRIPTION}" />
+                <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <link rel="icon" href="./{favicon.ico}" />
+                <link rel="stylesheet" href="./{css/style.css}" />
+              </head>
+              <body>
+                <main>
+                  {<h1>Hello, world!</h1>}
+                </main>
+                <script src="./{js/script.js}"></script>
+              </body>
+            </html>
+            --]]
+          ),
+        })
       end,
     },
     'saadparwaiz1/cmp_luasnip',
