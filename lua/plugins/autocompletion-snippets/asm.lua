3d4bcf789d8fa75147626b6dc9cf61d3cecf11c1
CupapiOT <166366567+CupapiOT@users.noreply.github.com>
2025-10-16 09:11:41 +0800

    Improve ASM snippets

--- lua/plugins/autocompletion-snippets/asm.lua
+++ lua/plugins/autocompletion-snippets/asm.lua
@@ -23,97 +23,122 @@ return {
     _start:
             $3
 
-            jmp end
-
     end:
-            ;   exit with code 0
-            mov rax, 60
+            ;   exit(error_code=0)
             xor rdi, rdi
+            mov rax, 60
             syscall
     ]]
   ),
   parse_snippet(
     'nasm-sys-exit',
     [[
-    ;   exit with code 0
-    mov rax, 60
+    ;   exit(error_code=0)
     xor rdi, rdi
+    mov rax, 60
     syscall
     ]]
+  ),
+  s(
+    'nasm-sys-write-stdout-print',
+    fmt(
+      [[
+      ;   write(fd=stdout, buf={}, count={})
+      mov rdi, 1
+      mov rsi, {}
+      mov rdx, {}
+      mov rax, 1
+      syscall
+      ]],
+      { rep(1), rep(2), i(1, 'buf'), i(2, 'count') }
+    )
+  ),
+  s(
+    'nasm-sys-read',
+    fmt(
+      [[
+      ;   read(fd={}, buf={}, count={})
+      mov rdi, {}
+      mov rsi, {}
+      mov rdx, {}
+      mov rax, 0
+      syscall
+      ]],
+      { rep(1), rep(2), rep(3), i(1, 'fd'), i(2, 'buf'), i(3, 'count') }
+    )
   ),
   s(
     'nasm-sys-write',
     fmt(
       [[
-      ;   write(stdout, {}, {})
+      ;   write(fd={}, buf={}, count={})
+      mov rdi, {}
+      mov rsi, {}
+      mov rdx, {}
       mov rax, 1
-      mov rdi, 1
-      mov rsi, {}
-      mov rdx, {}
       syscall
       ]],
-      { rep(1), rep(2), i(1, 'msg'), i(2, 'len') }
+      { rep(1), rep(2), rep(3), i(1, 'fd'), i(2, 'buf'), i(3, 'count') }
     )
   ),
   s(
-    'nasm-sys-openfile',
+    'nasm-sys-open',
     fmt(
       [[
-      ;   fopen({}, {}, {})
+      ;   open(pathname={}, flags={})
+      mov rdi, {}
+      mov rsi, {}
       mov rax, 2
-      mov rdi, {}
-      mov rsi, {}
       syscall
       ]],
-      { rep(1), rep(2), rep(3), i(1, 'filename'), i(2, 'mode') }
+      { rep(1), rep(2), i(1, 'pathname'), i(2, 'flags') }
     )
   ),
   s(
-    'nasm-sys-readfile',
+    'nasm-sys-lseek',
     fmt(
       [[
-      ;   fread({}, 1, {}, {})
+      ;   lseek(fd={}, offset={}, whence={})
       mov rdi, {}
-      mov rax, 0
       mov rsi, {}
       mov rdx, {}
+      mov rax, 8
       syscall
       ]],
-      { rep(2), rep(3), rep(1), i(1, 'fd'), i(2, 'buf'), i(3, 'count') }
+      { rep(1), rep(2), rep(3), i(1, 'fd'), i(2, 'offset'), i(3, 'whence') }
     )
   ),
 
-  -- AT&T Syntax
-  parse_snippet(
-    '!at&t-default',
-    [[
-    .section .data
-    $1
-    
-    .section .bss
-    $2
-
-    .section .text
-    .global  _start
-
-    _start:
-            $3
-            jmp end
-
-    end: 
-            //  exit with code 0
-            mov \$60, %rax
-            xor %rdi, %rdi
-            syscall
-    ]]
-  ),
-  parse_snippet(
-    '!at&t-sys-exit',
-    [[
-    //  exit with code 0;
-    mov \$60, %rax
-    xor %rdi, %rdi
-    syscall
-    ]]
-  ),
+  -- -- AT&T Syntax
+  -- parse_snippet(
+  --   '!at&t-default',
+  --   [[
+  --   .section .data
+  --   $1
+  --
+  --   .section .bss
+  --   $2
+  --
+  --   .section .text
+  --   .global  _start
+  --
+  --   _start:
+  --           $3
+  --
+  --   end:
+  --           //  exit with code 0
+  --           xor %rdi, %rdi
+  --           mov \$60, %rax
+  --           syscall
+  --   ]]
+  -- ),
+  -- parse_snippet(
+  --   '!at&t-sys-exit',
+  --   [[
+  --   //  exit with code 0;
+  --   xor %rdi, %rdi
+  --   mov \$60, %rax
+  --   syscall
+  --   ]]
+  -- ),
 }
