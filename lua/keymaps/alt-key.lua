525cbf0e54f984b4a3b8c04f2c6551a38d53387d
CupapiOT <166366567+CupapiOT@users.noreply.github.com>
2025-12-20 18:13:04 +0800

    Make linewrap wrap words, not just letters.
    I can't believe it took me this long to set this up.

--- lua/keymaps/alt-key.lua
+++ lua/keymaps/alt-key.lua
@@ -7,4 +7,4 @@ g_keymap('n', '<M-r>', ':mode<CR>', { desc = '[R]efresh the screen.', noremap = 
 g_keymap('n', '<M-s>', ':Sleuth<CR>', { desc = 'Trigger vim-[S]leuth', noremap = true, silent = true })
 
 -- Line-wrap toggle.
-g_keymap('n', '<M-z>', ':set wrap!<CR>', { desc = 'Toggle line wrap.', noremap = true, silent = true })
+g_keymap('n', '<M-z>', ':set wrap!<CR>:set linebreak<CR>', { desc = 'Toggle word wrap.', noremap = true, silent = true })
