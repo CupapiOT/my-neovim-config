471406552829a6056b0a491c1426006aec9185f9
CupapiOT <166366567+CupapiOT@users.noreply.github.com>
2026-03-26 07:34:11 +0800

    Delete redundant keymap for opening nvimtree
    Also, it's inferior to `\` since `\` is just 1 keypress and opens in a
    better way.

--- lua/keymaps/toggles.lua
+++ lua/keymaps/toggles.lua
@@ -1,8 +1,5 @@
 local g_keymap = vim.keymap.set -- "Global keymap."
 
--- Neotree Toggle
-g_keymap('n', '<leader>tt', ':Neotree toggle source=filesystem reveal=true position=left<CR>', { desc = '[T]oggle neo-[t]ree', noremap = true, silent = true })
-
 -- Lazygit Toggle
 g_keymap('n', '<leader>g', ':LazyGit<CR>', { desc = 'Toggle lazy[g]it.', noremap = true, silent = true })
 
