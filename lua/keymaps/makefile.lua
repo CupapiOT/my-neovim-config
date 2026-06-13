7f5af939437cdc6d5b47764edfa6222dcb331be7
CupapiOT <166366567+CupapiOT@users.noreply.github.com>
2026-06-13 11:46:40 +0800

    Add two more makefile keymaps

--- lua/keymaps/makefile.lua
+++ lua/keymaps/makefile.lua
@@ -9,3 +9,5 @@ g_keymap('n', '<leader>mb', KeymapCommand 'make build', { desc = '[M]ake [b]uild
 g_keymap('n', '<leader>md', KeymapCommand 'make debug', { desc = '[M]ake [d]ebug.', noremap = true })
 g_keymap('n', '<leader>mc', KeymapCommand 'make clean', { desc = '[M]ake [c]lean.', noremap = true })
 g_keymap('n', '<leader>mv', KeymapCommand 'make V=1', { desc = '[M]ake [v]erbose.', noremap = true })
+g_keymap('n', '<leader>mt', KeymapCommand 'make test', { desc = '[M]ake [t]est.', noremap = true })
+g_keymap('n', '<leader>m;', KeymapCommand('make ', false), { desc = '[M]ake ...', noremap = true })
